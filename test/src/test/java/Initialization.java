7b48664158cff9b964e1ef08d0e6617b5857fc03
rebak16 <rebakbalazs@gmail.com>
2020-04-14 16:48:51 +0200

    test important classes

--- test/src/test/java/Initialization.java
+++ test/src/test/java/Initialization.java
@@ -1,2 +1,22 @@
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+
 public class Initialization {
+
+    public WebDriver driver;
+
+    @BeforeEach
+    public void init() {
+        driver = WebDriverManager.getDriver();
+    }
+
+   /* @AfterEach
+    public void teardown() {
+        WebDriverManager.getDriver().close();
+        WebDriverManager.quit();
+    }*/
+
 }
